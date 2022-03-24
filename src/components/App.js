b66a1a9ce5d3acc7f71aaebeda3a4e9e27045873
Zach Liibbe <zliibbe@gmail.com>
2022-03-24 15:00:21 -0600

    Add constructor and initial JSX layout

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,26 @@
 import '../App.css';
+import movieData from '../movieData';
+import React from 'react';
+import MoviesContainer from './MoviesContainer'
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends React.Component {
+  constructor () {
+    super();
+    this.state = { movieData: movieData.movies };
+
+  }
+
+  render () {
+    return (
+      <main className="App">
+       <nav className="header">
+          <h1>Rancid Tomatillos</h1>
+        </nav>
+        <MoviesContainer  movieData={this.state.movieData}/> 
+      </main>
+    );
+  }
+
 }
 
 export default App;
