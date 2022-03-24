b709ab0b9523efaa8904032d20336fb9b761f304
Zach Liibbe <zliibbe@gmail.com>
2022-03-24 15:15:50 -0600

    Add MoviesContainer class

---
+++ src/components/MoviesContainer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import MovieOverview from 'MovieOverview';
+
+const MoviesContainer = (movieData) => {
+    const movies = movieData.map(movie => {
+        return (
+            <MovieOverview key={movie.id} id={movie.id} posterPath={movie.poster_path} title={movie.title}/>
+        )
+    })
+
+    return (<section className="movie-container">{movies}</section>)
+}
+
+export default MoviesContainer;
