77d135f64aca98030446e5bcd3f0211b1d0ea6f9
Zach Liibbe <zliibbe@gmail.com>
2022-03-24 20:24:13 -0600

    Add MovieOverview class component with render method

---
+++ src/components/MovieOverview.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class MovieOverview extends React.Component {
+    constructor ({ key, id, posterPath, title}) {
+        this.state = {
+            id: id,
+            title: title,
+            posterPath: posterPath,
+        }
+    }
+
+    render() {
+        return (
+        <div className='movie-card'>
+            <img src={this.state.posterPath}/>
+            <h1>{this.state.title}</h1>
+        </div>
+        )
+    }
+}
+
+export default MovieOverview;
