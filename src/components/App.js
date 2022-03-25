d257aaf6d07e5090b9da800c7656590baae18992
Madison Law <madisonlaw1@gmail.com>
2022-03-25 15:37:32 -0600

    Create displaySingleMovie function

--- src/components/App.js
+++ src/components/App.js
@@ -6,8 +6,18 @@ import MoviesContainer from './MoviesContainer'
 class App extends React.Component {
   constructor () {
     super();
-    this.state = { movieData: movieData.movies };
+    this.state = { 
+      movieData: movieData.movies,
+      currentMovie: null
+    };
+  }
 
+  displaySingleMovie = (id) => {
+    let foundMovie = this.state.movieData.find(movie => movie.id === id);
+    this.setState({currentMovie: foundMovie})
+    // needs to be able to find a movie based on id and maybe set state for a
+    // currentMovie property in App? Then App can render either the 
+    // MoviesContainer or the MovieDetail depending on whether that property exists
   }
 
   render () {
@@ -16,7 +26,7 @@ class App extends React.Component {
         <nav className="header">
           <h1>Rancid Tomatillos</h1>
         </nav>
-        <MoviesContainer  movieData={this.state.movieData}/> 
+        <MoviesContainer  movieData={this.state.movieData} displaySingleMovie={this.displaySingleMovie}/> 
       </main>
     );
   }
