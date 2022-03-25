1d18dc7464aa7dc02188e4c9869e7679ae29f5b0
Madison Law <madisonlaw1@gmail.com>
2022-03-25 17:22:31 -0600

    Add sections to JSX for layout

--- src/components/MovieDetail.js
+++ src/components/MovieDetail.js
@@ -12,16 +12,33 @@ class MovieDetail extends React.Component {
 
   render() {
     return (
-      <section className='single-movie-section'>
-        <h1 className='movie-title'>{this.state.movieDetails.title}</h1>
-        <img className='poster-img' src={this.state.movieDetails.poster_path} alt="Movie poster" />
-        <p className='avg-rating'>{this.state.movieDetails.average_rating}</p>
-        <p className='release-date'>{this.state.movieDetails.release_date}</p>
-        <img className='backdrop-img' src={this.state.movieDetails.backdrop_path} alt="Movie backdrop" />
-        <button className='back-to-main' onClick={() => this.state.displayMainDashboard()}>X</button>
-      </section>
+      <main className='single-movie-section' style={{backgroundImage: `url(${this.state.movieDetails.backdrop_path})`, 'background-repeat': 'no-repeat', 'background-size': 'cover'}}>
+        <div className='single-movie-container'>
+
+          <section className='movie-main'>
+
+            <div className='poster-rating-container'>
+              <img className='poster-img' src={this.state.movieDetails.poster_path} alt="Movie poster" />
+              <p className='avg-rating'>{this.state.movieDetails.average_rating}</p>
+            </div>
+
+            <div className='title-description-container'>
+              <h1 className='movie-title'>{this.state.movieDetails.title}</h1>
+              <p className='movie-overview'>Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem Ipsum</p>
+            </div>
+            
+
+          </section>
+          <section className='movie-details'>
+            <p className='release-date'>{this.state.movieDetails.release_date}</p>
+          </section>
+        </div>
+        <div className='back-to-main-container'>
+          <button className='back-to-main' onClick={() => this.state.displayMainDashboard()}>X</button>
+        </div>
+        {/* <img className='backdrop-img' src={this.state.movieDetails.backdrop_path} alt="Movie backdrop" /> */}
+      </main>
     )
-  
   }
 }
 
