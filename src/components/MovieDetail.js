a58b8ef9e48a591240b670a225e4034f49bbc949
Madison Law <madisonlaw1@gmail.com>
2022-03-25 17:53:37 -0600

    Add section for genre details

--- src/components/MovieDetail.js
+++ src/components/MovieDetail.js
@@ -12,7 +12,7 @@ class MovieDetail extends React.Component {
 
   render() {
     return (
-      <main className='single-movie-section' style={{backgroundImage: `url(${this.state.movieDetails.backdrop_path})`, 'background-repeat': 'no-repeat', 'background-size': 'cover'}}>
+      <main className='single-movie-section' style={{backgroundImage: `url(${this.state.movieDetails.backdrop_path})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover'}}>
         <div className='single-movie-container'>
 
           <section className='movie-main'>
@@ -24,19 +24,22 @@ class MovieDetail extends React.Component {
 
             <div className='title-description-container'>
               <h1 className='movie-title'>{this.state.movieDetails.title}</h1>
-              <p className='movie-overview'>Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem Ipsum</p>
+              <p className='movie-overview'>New Seasons organic dog parks stumptown gluten free mustachioed bartenders microbrew freegans breweries Alberta Arts local honey gentrification stumptown pearl district outdoorsy. Kristian Foden-Vencil beard 82nd Avenue of Roses Silicon Forest clouds stripclubs vibrant Impossible Burger faux bacon Plaid Pantry farm to table it's raining again Voodoo Donuts food carts Cascadia a dog gym. </p>
+              <div className='genre-section'>
+                <p className='release-date'>{this.state.movieDetails.release_date}</p>
+                <p>⎮</p>
+                <p>Genre goes here</p>
+                <p>⎮</p>
+                <p>2h 23m runtime</p>
+              </div>
             </div>
-            
-
+          
           </section>
-          <section className='movie-details'>
-            <p className='release-date'>{this.state.movieDetails.release_date}</p>
-          </section>
+  
         </div>
         <div className='back-to-main-container'>
           <button className='back-to-main' onClick={() => this.state.displayMainDashboard()}>X</button>
         </div>
-        {/* <img className='backdrop-img' src={this.state.movieDetails.backdrop_path} alt="Movie backdrop" /> */}
       </main>
     )
   }
