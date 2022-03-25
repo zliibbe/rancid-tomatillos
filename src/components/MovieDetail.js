e437c6cb6811c912aaa9897bd6c8d68d0d3175db
Madison Law <madisonlaw1@gmail.com>
2022-03-25 16:17:46 -0600

    Flesh out JSX and create button to return to main

--- src/components/MovieDetail.js
+++ src/components/MovieDetail.js
@@ -1,16 +1,24 @@
 import React from 'react';
 
 class MovieDetail extends React.Component {
-  constructor( { movieDetails }) {
+  constructor( { movieDetails, displayMainDashboard }) {
     super();
     this.state = {
-      movieDetails: movieDetails
+      movieDetails: movieDetails,
+      displayMainDashboard: displayMainDashboard
     }
   }
 
   render() {
     return (
-      <p>{this.state.movieDetails.title}</p>
+      <section className='single-movie-section'>
+        <h1>{this.state.movieDetails.title}</h1>
+        <img className='poster-img' src={this.state.movieDetails.poster_path} alt="Movie poster" />
+        <p className='avg-rating'>{this.state.movieDetails.average_rating}</p>
+        <p className='release-date'>{this.state.movieDetails.release_date}</p>
+        <img className='backdrop-img' src={this.state.movieDetails.backdrop_path} alt="Movie backdrop" />
+        <button className='back-to-main' onClick={() => this.state.displayMainDashboard()}>X</button>
+      </section>
     )
   
   }
