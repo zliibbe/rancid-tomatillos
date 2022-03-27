990a632b7f96cb5a1dc601418a54f02ec6c92a75
Zach Liibbe <zliibbe@gmail.com>
2022-03-27 15:23:11 -0600

    Fix conditional rendering & import DayJS

--- src/components/MovieDetail.js
+++ src/components/MovieDetail.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../styles/MovieDetail.css';
 import { fetchSingleMovie } from '../apiCalls';
+import DayJS from 'react-dayjs';
 
 class MovieDetail extends React.Component {
   constructor( { movieDetails, displayMainDashboard }) {
@@ -29,24 +30,20 @@ componentDidMount = () => {
 
             <div className='poster-rating-container'>
               <img className='poster-img' src={this.state.movieDetails.poster_path} alt="Movie poster" />
-              <p className='avg-rating'>Rating: {Math.round(this.state.movieDetails.average_rating * 10)/10} / 10</p>
+              <p className='avg-rating'><i>Rating: </i>{Math.round(this.state.movieDetails.average_rating * 10)/10} / 10</p>
             </div>
-
+            
             <div className='title-description-container'>
               <h1 className='movie-title'>{this.state.movieDetails.title}</h1>
-              {!this.state.movieDetails.overview && <p className='movie-overview'>No Description Available</p>}
-              <p className='movie-overview'>{this.state.movieDetails.overview}</p>
-              <div className='genre-section'>
-                <p className='release-date'>{this.state.movieDetails.release_date}</p>
-                <p>⎮</p>
+              {!this.state.movieDetails.overview ? <p className='movie-overview'>No Description Available</p> : <p className='movie-overview'>{this.state.movieDetails.overview}</p>}
+              
+              <div className='movie-details-section'>
+                <p className='release-date'><i>Released:</i> <DayJS format="MMMM DD, YYYY">{this.state.movieDetails.release_date}</DayJS></p>
                 <p>{this.state.movieDetails.genres}</p>
-                <p>⎮</p>
-                {!this.state.movieDetails.runtime && <p>No runtime available</p>}
-                {this.state.movieDetails.runtime != 0 && <p>{this.state.movieDetails.runtime} minutes</p>}
-              </div>
-              <div className='budget-revenue'>
-                {this.state.movieDetails.budget != 0 && <p className='budget'>Budget: {this.state.movieDetails.budget}</p>}
-                {this.state.movieDetails.revenue != 0 && <p className='revenue'>Revenue: {this.state.movieDetails.revenue}</p>}
+                {!this.state.movieDetails.runtime && <p className='runtime'>No runtime available</p>}
+                {this.state.movieDetails.runtime != 0 && <p className='runtime'><i>Runtime: </i> {this.state.movieDetails.runtime} minutes</p>}
+                {this.state.movieDetails.budget != 0 && <p className='budget'><i>Budget: </i>{this.state.movieDetails.budget}</p>}
+                {this.state.movieDetails.revenue != 0 && <p className='revenue'><i>Revenue:</i> {this.state.movieDetails.revenue}</p>}
               </div>
             </div>
           
