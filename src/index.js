f76354c1529957a3933c719160027aca57fb6889
Madison Law <madisonlaw1@gmail.com>
2022-03-28 16:38:12 -0600

    Install React-Router and set up in index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router} from 'react-router-dom';
 import './index.css';
 import App from './components/App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const router = 
+  <Router> 
+    <React.StrictMode> 
+      <App /> 
+    </React.StrictMode> 
+  </Router>
+
+ReactDOM.render(router, document.getElementById('root'));
 
