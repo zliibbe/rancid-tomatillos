da53a537e7555f4623858493b181b84df1030f37
Madison Law <madisonlaw1@gmail.com>
2022-03-28 20:48:15 -0600

    Remove onClick handler for displaying single movie

--- src/components/MovieOverview.js
+++ src/components/MovieOverview.js
@@ -15,7 +15,7 @@ class MovieOverview extends React.Component {
 
     render() {
         return (
-        <div className='movie-card' tabIndex='1' onClick={() => this.state.displaySingleMovie(this.state.id)}>
+        <div className='movie-card' tabIndex='1'>
             <Link to={`/${this.state.id}`} style={{ textDecoration: 'none' }}>
                 <img src={this.state.posterPath}/>
                 <h1 className='movie-title-poster'>{this.state.title}</h1>
@@ -24,5 +24,6 @@ class MovieOverview extends React.Component {
         )
     }
 }
+
 
 export default MovieOverview;
