f4657157b996c3f9e0c3fc6eed0c5d3599680e46
Zach Liibbe <zliibbe@gmail.com>
2022-03-30 14:11:16 -0600

    Add .intercept to cypress tests

--- cypress/integration/movie_display_spec.js
+++ cypress/integration/movie_display_spec.js
@@ -1,5 +1,27 @@
 describe('Main movie view', () => {
     it('Should show user a dashboard with sorted movies displayed', () => {
+       
+        cy.intercept("GET", "https://rancid-tomatillos.herokuapp.com/api/v2/movies/", 
+            {
+            "movies": [
+            {
+                "id": 528085,
+                "poster_path": "https://image.tmdb.org/t/p/original//7D430eqZj8y3oVkLFfsWXGRcpEG.jpg",
+                "backdrop_path": "https://image.tmdb.org/t/p/original//5UkzNSOK561c2QRy2Zr4AkADzLT.jpg",
+                "title": "2067",
+                "average_rating": 5.166666666666667,
+                "release_date": "2020-10-01"
+              },
+              {
+                "id": 613504,
+                "poster_path": "https://image.tmdb.org/t/p/original//kiX7UYfOpYrMFSAGbI6j1pFkLzQ.jpg",
+                "backdrop_path": "https://image.tmdb.org/t/p/original//r5srC0cqU36n38azFnCyReEksiR.jpg",
+                "title": "After We Collided",
+                "average_rating": 5.25,
+                "release_date": "2020-09-02"
+              }
+        ]}
+      )
         cy.visit('http://localhost:3000/')
         .contains('Rancid Tomatillos')
         .get('.movie-card')
@@ -7,6 +29,26 @@ describe('Main movie view', () => {
     });
 
     it('shows an individual movie\'s details when a user clicks on a movie poster', () => {
+        cy.intercept(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/`, {
+        "movies": [
+            {
+                "id": 528085,
+                "poster_path": "https://image.tmdb.org/t/p/original//7D430eqZj8y3oVkLFfsWXGRcpEG.jpg",
+                "backdrop_path": "https://image.tmdb.org/t/p/original//5UkzNSOK561c2QRy2Zr4AkADzLT.jpg",
+                "title": "2067",
+                "average_rating": 5.166666666666667,
+                "release_date": "2020-10-01"
+              },
+              {
+                "id": 613504,
+                "poster_path": "https://image.tmdb.org/t/p/original//kiX7UYfOpYrMFSAGbI6j1pFkLzQ.jpg",
+                "backdrop_path": "https://image.tmdb.org/t/p/original//r5srC0cqU36n38azFnCyReEksiR.jpg",
+                "title": "After We Collided",
+                "average_rating": 5.25,
+                "release_date": "2020-09-02"
+              }
+        ]
+      })
         cy.visit('http://localhost:3000/')
         .get('.movie-card')
         .first()
@@ -16,6 +58,26 @@ describe('Main movie view', () => {
     })
 
     it('allows the user to return to main dashboard after viewing an individual movie', () => {
+        cy.intercept(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/`, {
+        "movies": [
+            {
+                "id": 528085,
+                "poster_path": "https://image.tmdb.org/t/p/original//7D430eqZj8y3oVkLFfsWXGRcpEG.jpg",
+                "backdrop_path": "https://image.tmdb.org/t/p/original//5UkzNSOK561c2QRy2Zr4AkADzLT.jpg",
+                "title": "2067",
+                "average_rating": 5.166666666666667,
+                "release_date": "2020-10-01"
+              },
+              {
+                "id": 613504,
+                "poster_path": "https://image.tmdb.org/t/p/original//kiX7UYfOpYrMFSAGbI6j1pFkLzQ.jpg",
+                "backdrop_path": "https://image.tmdb.org/t/p/original//r5srC0cqU36n38azFnCyReEksiR.jpg",
+                "title": "After We Collided",
+                "average_rating": 5.25,
+                "release_date": "2020-09-02"
+              }
+        ]
+      })
         cy.visit('http://localhost:3000/')
         .get('.movie-card')
         .first()
@@ -27,6 +89,26 @@ describe('Main movie view', () => {
     })
     
     it('navigates to a unique URL for each movie selected', () => {
+        cy.intercept(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/`, {
+        "movies": [
+            {
+                "id": 528085,
+                "poster_path": "https://image.tmdb.org/t/p/original//7D430eqZj8y3oVkLFfsWXGRcpEG.jpg",
+                "backdrop_path": "https://image.tmdb.org/t/p/original//5UkzNSOK561c2QRy2Zr4AkADzLT.jpg",
+                "title": "2067",
+                "average_rating": 5.166666666666667,
+                "release_date": "2020-10-01"
+              },
+              {
+                "id": 613504,
+                "poster_path": "https://image.tmdb.org/t/p/original//kiX7UYfOpYrMFSAGbI6j1pFkLzQ.jpg",
+                "backdrop_path": "https://image.tmdb.org/t/p/original//r5srC0cqU36n38azFnCyReEksiR.jpg",
+                "title": "After We Collided",
+                "average_rating": 5.25,
+                "release_date": "2020-09-02"
+              }
+        ]
+      })
         cy.visit('http://localhost:3000/')
         .get('.movie-card')
         .first()
@@ -36,6 +118,27 @@ describe('Main movie view', () => {
     })
     
     it('navigates to base URL when exiting single movie view', () => {
+        cy.intercept(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/`, {
+        "movies": [
+            {
+                "id": 528085,
+                "poster_path": "https://image.tmdb.org/t/p/original//7D430eqZj8y3oVkLFfsWXGRcpEG.jpg",
+                "backdrop_path": "https://image.tmdb.org/t/p/original//5UkzNSOK561c2QRy2Zr4AkADzLT.jpg",
+                "title": "2067",
+                "average_rating": 5.166666666666667,
+                "release_date": "2020-10-01"
+              },
+              {
+                "id": 613504,
+                "poster_path": "https://image.tmdb.org/t/p/original//kiX7UYfOpYrMFSAGbI6j1pFkLzQ.jpg",
+                "backdrop_path": "https://image.tmdb.org/t/p/original//r5srC0cqU36n38azFnCyReEksiR.jpg",
+                "title": "After We Collided",
+                "average_rating": 5.25,
+                "release_date": "2020-09-02"
+              }
+        ]
+      })
+
         cy.visit('http://localhost:3000/')
         .get('.movie-card')
         .first()
@@ -44,5 +147,28 @@ describe('Main movie view', () => {
         .click()
         .url()
         .should('include', '/')
+    })
+
+    it('', () => {
+        cy.intercept(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/`, {
+        "movies": [
+            {
+                "id": 528085,
+                "poster_path": "https://image.tmdb.org/t/p/original//7D430eqZj8y3oVkLFfsWXGRcpEG.jpg",
+                "backdrop_path": "https://image.tmdb.org/t/p/original//5UkzNSOK561c2QRy2Zr4AkADzLT.jpg",
+                "title": "2067",
+                "average_rating": 5.166666666666667,
+                "release_date": "2020-10-01"
+              },
+              {
+                "id": 613504,
+                "poster_path": "https://image.tmdb.org/t/p/original//kiX7UYfOpYrMFSAGbI6j1pFkLzQ.jpg",
+                "backdrop_path": "https://image.tmdb.org/t/p/original//r5srC0cqU36n38azFnCyReEksiR.jpg",
+                "title": "After We Collided",
+                "average_rating": 5.25,
+                "release_date": "2020-09-02"
+              }
+        ]
+      })
     })
   });
