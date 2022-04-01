c26beada449dbb88bbb22b90db42f22a82491941
Madison Law <madisonlaw1@gmail.com>
2022-04-01 13:48:49 -0600

    Create componentDidMount and mapping method for movies

--- src/components/StarredMoviesContainer.js
+++ src/components/StarredMoviesContainer.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import '../styles/StarredMoviesContainer.css';
 import { Link } from "react-router-dom";
-
+import { fetchStarredMovies } from "../apiCalls";
+import MovieOverview from './MovieOverview';
 
 class StarredMoviesContainer extends React.Component {
   constructor() {
@@ -9,8 +10,30 @@ class StarredMoviesContainer extends React.Component {
     this.state = {
       starredMovies: []
     }
-
   }
+
+  mapStarredMovies = () => {
+    this.state.starredMovies.map(movie => {
+      return (
+        <MovieOverview key={movie.id} id={movie.id} posterPath={movie.poster_path} title={movie.title} />
+      )
+    })
+  }
+
+
+
+  componentDidMount = () => {
+    fetchStarredMovies()
+      .then((data) => {
+        this.props.sortByTitle(data.movies);
+        this.setState({ starredMovies: data.movies})
+      })
+      .catch((error) => {
+        this.props.errorHandling();
+        console.warn(error);
+      });
+  }
+
   render() {
     return (
       <main className='starred-movie-main'>
