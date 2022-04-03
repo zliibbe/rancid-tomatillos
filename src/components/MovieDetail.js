a73903c4b06062aec7acb797edde2629526c42d1
Madison Law <madisonlaw1@gmail.com>
2022-04-03 15:46:50 -0600

    Add conditional rendering for star button & update genre formatter

--- src/components/MovieDetail.js
+++ src/components/MovieDetail.js
@@ -25,10 +25,8 @@ class MovieDetail extends React.Component {
   }
 
   formatGenres = (genres) => {
-    let genreList = genres.reduce((list, genre) => list += `${genre}, `, '')
-    return genreList.slice(0, genreList.length - 2)
+    return genres.join(', ')
   }
-
 
   postNewFavorite = () => {
     const movieToStar = {
@@ -38,12 +36,11 @@ class MovieDetail extends React.Component {
     }
     postStarredMovie(movieToStar)
     .catch(error => {
-      this.setState({ error: error.message, isStarred: false})
+      this.setState({ error: error.message, isStarred: false })
     })
 
     this.setState({isStarred: true})
   }
-
 
   componentDidMount = () => {
     fetchSingleMovie(this.state.movieID)
@@ -62,22 +59,19 @@ class MovieDetail extends React.Component {
     return (
       <main className='single-movie-section' style={{backgroundImage: `url(${this.state.movieDetails.backdrop_path})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover'}}>
         <div className='single-movie-container'>
-
           <section className='movie-main'>
-
             <div className='poster-rating-container'>
               <img className='poster-img' src={this.state.movieDetails.poster_path} alt="Movie poster" />
               <div className='rating-star-container'>
                 <p className='avg-rating'><i>Average Rating: </i>{Math.round(this.state.movieDetails.average_rating * 10)/10} / 10</p>
-                <button className='star-movie-btn' type='button' onClick={() => this.postNewFavorite()}><svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"></path></svg></button>
-
+                {this.state.isStarred && <button className='star-movie-btn' type='button' onClick={() => this.postNewFavorite()}><svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg></button>}
+                {!this.state.isStarred && <button className='star-movie-btn' type='button' onClick={() => this.postNewFavorite()}><svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"></path></svg></button>}
+                {this.state.error && <p>You've already favorited this movie!</p>}
               </div>
             </div>
-            
             <div className='title-description-container'>
               <h1 className='movie-title'>{this.state.movieDetails.title}</h1>
               {!this.state.movieDetails.overview ? <p className='movie-overview'>No Description Available</p> : <p className='movie-overview'>{this.state.movieDetails.overview}</p>}
-              
               <div className='movie-details-section'>
                 <p className='release-date'><i>Released:</i> <DayJS format="MMMM DD, YYYY">{this.state.movieDetails.release_date}</DayJS></p>
                 {this.state.movieDetails.genres && <p><i>Genres: </i>{this.formatGenres(this.state.movieDetails.genres)}</p>}
@@ -86,9 +80,7 @@ class MovieDetail extends React.Component {
                 {this.state.movieDetails.revenue != 0 && <p className='revenue'>Revenue: {this.formatCurrency(this.state.movieDetails.revenue)}</p>}
               </div>
             </div>
-          
           </section>
-  
         </div>
         <div className='back-to-main-container'>
           <Link to='/' style={{ textDecoration: 'none' }}>
@@ -98,7 +90,6 @@ class MovieDetail extends React.Component {
       </main>
     )
   }
-
 }
 
 
