33cf8b928db44ed608054f098c3285bd99b33eb4
Zach Liibbe <zliibbe@gmail.com>
2022-04-04 16:56:58 -0600

    Run prettier on all files and add favicon

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,29 +1,29 @@
 import { render } from "@testing-library/react";
 
 const fetchMovies = (url) => {
-        return fetch(url)
-        .then(response => {
-        if (!response.ok) {
-            throw new Error('Error with requesting movies.');
-          } else {
-           return response.json();
-          }
-        })
-}
+  return fetch(url).then((response) => {
+    if (!response.ok) {
+      throw new Error("Error with requesting movies.");
+    } else {
+      return response.json();
+    }
+  });
+};
 
 const postStarredMovie = (movieToStar) => {
-  return fetch('http://localhost:3001/api/v1/starredMovies', {
-    method: 'POST',
+  return fetch("http://localhost:3001/api/v1/starredMovies", {
+    method: "POST",
     body: JSON.stringify(movieToStar),
-    headers: {'Content-Type': 'application/json'}
-  })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error ('This movie has already been favorited or is missing a parameter');
-      } else {
-        return response.json()
-      }
-    })
-}
+    headers: { "Content-Type": "application/json" },
+  }).then((response) => {
+    if (!response.ok) {
+      throw new Error(
+        "This movie has already been favorited or is missing a parameter"
+      );
+    } else {
+      return response.json();
+    }
+  });
+};
 
-export { fetchMovies, postStarredMovie }
+export { fetchMovies, postStarredMovie };
--- src/components/MovieOverview.js
+++ src/components/MovieOverview.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import '../styles/MovieOverview.css'
-import { Link } from 'react-router-dom';
+import React from "react";
+import "../styles/MovieOverview.css";
+import { Link } from "react-router-dom";
 
 const MovieOverview = ({ id, posterPath, title }) => {
-    return (
-        <div className='movie-card' tabIndex='1'>
-            <Link to={`/movies/${id}`} style={{ textDecoration: 'none' }}>
-                <img className='movie-card-poster' src={posterPath}/>
-                <h1 className='movie-title-poster'>{title}</h1>
-            </Link>
-        </div>
-    )
-}
+  return (
+    <div className="movie-card" tabIndex="1">
+      <Link to={`/movies/${id}`} style={{ textDecoration: "none" }}>
+        <img className="movie-card-poster" src={posterPath} />
+        <h1 className="movie-title-poster">{title}</h1>
+      </Link>
+    </div>
+  );
+};
 
 export default MovieOverview;
