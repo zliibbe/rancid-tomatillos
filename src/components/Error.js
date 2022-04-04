95412e46bd1df2aedabfd254d5157fbfaa20091f
Zach Liibbe <zliibbe@gmail.com>
2022-04-04 11:17:24 -0600

    Refactor error for 404 error msg

--- src/components/Error.js
+++ src/components/Error.js
@@ -2,14 +2,14 @@ import React from "react";
 import { Link } from 'react-router-dom';
 import '../styles/Error.css'
 
-const Error = ( { resetError } ) => {
+const Error = ( { resetError, message } ) => {
     return (
         <main className="error-container">
             <nav className="header"><h1>Rancid Tomatillos</h1></nav>
-            <h1 className="error">Something went wrong</h1>
-            <Link to='/'>
+            <h1 className="error">{message}</h1>
+            {message === '404: The URL you entered is not valid.' && <Link to='/'>
                 <button type='button' onClick={() => resetError()}>Back to Main</button>
-            </Link>
+            </Link>}
         </main>
     )
 } 
